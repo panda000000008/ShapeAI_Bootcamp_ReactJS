c9a39a63817deb49c35465eb6b75c6e60770715d
panda000000008 <86357247+panda000000008@users.noreply.github.com>
2021-06-28 09:33:10 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Python and Network Security</h1>
+        <p>Instructor - Harsh Akshit</p>
+        <p>Duration - 7 Days(Starts from 27th June)</p>
+        <p>ShapeAI provides :-</p>
+        <ul type="none">
+          <li>Certificates</li>
+          <li>Hand on projects</li>
+          <li>No prerequisites</li>
+        </ul>
+      </div>
+      <div className="note">
+        <h1>Javascript and Backend with NodeJS</h1>
+        <p>Instructor - Shaurya Sinha</p>
+        <p>Duration - 7 Days(Starts from 27th June)</p>
+        <p>ShapeAI provides :-</p>
+        <ul type="none">
+          <li>Certificates</li>
+          <li>Hand on projects</li>
+          <li>No prerequisites</li>
+        </ul>
+      </div>
+      <div className="note">
+        <h1>Python and Natural Language Processing</h1>
+        <p>Instructor - Madhav</p>
+        <p>Duration - 7 Days(Starts from 27th June)</p>
+        <p>ShapeAI provides :-</p>
+        <ul type="none">
+          <li>Certificates</li>
+          <li>Hand on projects</li>
+          <li>No prerequisites</li>
+        </ul>
+      </div>
+      <div className="note">
+        <h1>Tableau Fundamentals</h1>
+        <p>Instructor - Dipayan Ghoshal</p>
+        <p>Duration - 7 Days(Starts from 27th June)</p>
+        <p>ShapeAI provides :-</p>
+        <ul type="none">
+          <li>Certificates</li>
+          <li>Hand on projects</li>
+          <li>No prerequisites</li>
+        </ul>
+      </div>
+      <div className="note">
+        <h1>Python and Data Visualization</h1>
+        <p>Instructor - Shaurya Sinha</p>
+        <p>Duration - 7 Days(Starts from 27th June)</p>
+        <p>ShapeAI provides :-</p>
+        <ul type="none">
+          <li>Certificates</li>
+          <li>Hand on projects</li>
+          <li>No prerequisites</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
